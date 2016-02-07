c949c9074cb7ec9319a731038fe8fe240c12b339
Kazuto Seito <torinokonoko@gmail.com>
2016-02-07 15:56:44 +0900

    implement with rxjava

--- app/src/main/java/kztproject/com/retrofitpractice/MainActivity.java
+++ app/src/main/java/kztproject/com/retrofitpractice/MainActivity.java
@@ -3,17 +3,19 @@ package kztproject.com.retrofitpractice;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.List;
 
 import kztproject.com.retrofitpractice.models.GourmetData;
 import kztproject.com.retrofitpractice.models.Shop;
-import retrofit2.Call;
-import retrofit2.Callback;
 import retrofit2.GsonConverterFactory;
-import retrofit2.Response;
 import retrofit2.Retrofit;
+import retrofit2.RxJavaCallAdapterFactory;
+import rx.Observable;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Func1;
+import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,28 +26,43 @@ public class MainActivity extends AppCompatActivity {
 
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("http://webservice.recruit.co.jp")
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
         HotPepperService service = retrofit.create(HotPepperService.class);
 
-        Call<GourmetData> call = service.getGourmetData(Environment.API_KEY, "Z012", "json");
-        call.enqueue(new Callback<GourmetData>() {
-            @Override
-            public void onResponse(Response<GourmetData> response) {
-                List<Shop> shopList = response.body().results.getShop();
-                StringBuilder builder = new StringBuilder();
+        Observable<GourmetData> observable = service.getGourmetData(Environment.API_KEY, "Z012", "json");
+        observable
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .map(new Func1<GourmetData, List<Shop>>() {
+                    @Override
+                    public List<Shop> call(GourmetData gourmetData) {
+                        return gourmetData.results.getShop();
+                    }
+                })
+                .subscribe(new Subscriber<List<Shop>>() {
+                    @Override
+                    public void onCompleted() {
+                        System.out.println("Completed!");
+                    }
 
-                for (Shop shop : shopList) {
-                    builder.append(shop.getName() + "\n");
-                }
+                    @Override
+                    public void onError(Throwable e) {
+                        System.out.println("Error:" + e.getMessage());
+                    }
+
+                    @Override
+                    public void onNext(List<Shop> shops) {
+                        StringBuilder builder = new StringBuilder();
+
+                        for (Shop shop : shops) {
+                            builder.append(shop.getName() + "\n");
+                        }
                         ((TextView) findViewById(R.id.textview)).setText(new String(builder));
-            }
+                    }
+                });
 
-            @Override
-            public void onFailure(Throwable t) {
-                Toast.makeText(getApplicationContext(), t.getMessage(), Toast.LENGTH_LONG).show();
-            }
-        });
     }
 }
